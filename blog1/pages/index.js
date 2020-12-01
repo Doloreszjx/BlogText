761a4d59a35bfe1f8fe2fb77bc775d190cdf6f89
doloreszjx <1173827505@qq.com>
2020-12-01 22:16:32 +0800

    components: header

---
+++ blog1/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "next/head";
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home</title>
+      </Head>
+      <Header />
+    </>
+  );
+}
