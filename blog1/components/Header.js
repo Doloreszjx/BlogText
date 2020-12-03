4653c325af1d535cbc512b5f54051ace0235cc98
doloreszjx <1173827505@qq.com>
2020-12-03 22:58:01 +0800

    主页左侧列表项

--- blog1/components/Header.js
+++ blog1/components/Header.js
@@ -1,35 +1,33 @@
 import React from "react";
 
 import "../static/style/components/Header.css";
-import { Row, Col, Menu, Icon } from "antd";
-import MenuItem from "antd/lib/menu/MenuItem";
+import { Row, Col, Menu } from "antd";
 
-const Header = () => (
-  <div className="header">
-    <Row type="flex" justify="center">
-      <Col xs={24} sm={24} md={10} lg={10} xl={10}>
-        <span className="header-logo">技术胖</span>
-        <span className="header-txt">专注前端开发,每年100集免费视频。</span>
-      </Col>
+const Header = (props) => {
+  const { logo, text, menu } = props;
 
-      <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
-        <Menu mode="horizontal">
-          <Menu.Item key="home">
-            <Icon type="home" />
-            首页
-          </Menu.Item>
-          <Menu.Item key="video">
-            <Icon type="youtube" />
-            视频
-          </Menu.Item>
-          <Menu.Item key="life">
-            <Icon type="smile" />
-            生活
-          </Menu.Item>
-        </Menu>
-      </Col>
-    </Row>
-  </div>
-);
+  return (
+    <div className="header-wrapper">
+      <Row type="flex" justify="center">
+        <Col xs={24} sm={24} md={10} lg={15} xl={12}>
+          <span className="header-logo">{logo}</span>
+          <span className="header-txt">{text}</span>
+        </Col>
+
+        <Col className="memu-div" xs={0} sm={0} md={14} lg={8} xl={6}>
+          <Menu mode="horizontal">
+            {menu.map((item) => {
+              return (
+                <Menu.Item key={item.key} className="header-menuItem">
+                  {item.desc}
+                </Menu.Item>
+              );
+            })}
+          </Menu>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Header;
---
+++ blog1/pages/detail.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { Row, Col } from "antd";
+
+export default function detail() {
+  return (
+    <Row className="home-main" type="flex" justify="center">
+      <Col className="home-left" xs={24} sm={24} md={16} lg={18} xl={14}>
+        left
+      </Col>
+      <Col className="home-right" xs={0} sm={0} md={7} lg={5} xl={4}>
+        right
+      </Col>
+    </Row>
+  );
+}
